01749144e218507a409a3d69cf1c4193f1c3b5b2
Kunal Malviya <kunal.malviya351@gmail.com>
2019-06-29 11:20:22 +0530

    fixed the email message

--- simple-job-listings.php
+++ simple-job-listings.php
@@ -539,7 +539,7 @@ function action_request_lead_details_callback() {
 		$approve_lead_info_message = str_replace("{{clientMobile}}", $client_telephone_number, $approve_lead_info_message);
 
 		$reject_lead_info_message = get_option("reject_lead_info_message", "reject lead info message");
-		$approve_lead_info_message = str_replace("{{jobTitle}}", $postTitle, $reject_lead_info_message);	
+		$reject_lead_info_message = str_replace("{{jobTitle}}", $postTitle, $reject_lead_info_message);
 		$reject_lead_info_message = str_replace("{{clientName}}", $client_name, $reject_lead_info_message);
 		$reject_lead_info_message = str_replace("{{clientAddress}}", $client_address, $reject_lead_info_message);
 		$reject_lead_info_message = str_replace("{{clientEmail}}", $client_email, $reject_lead_info_message);
@@ -571,7 +571,7 @@ function action_request_lead_details_callback() {
 
 		$arrayToStore = array_unique($arrayToStore);
 		$updateuserMetaFlag = update_post_meta( $postId, $string, json_encode( array_values($arrayToStore) ) );		
-			
+				
 		wp_mail($currentUserEmail, $subject, $emailBody);
 	}
 	wp_die();
